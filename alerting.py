0da2ee4e305c13135edfe4fc629fbe0e3418f1ce
T1 <126980610+tadash10@users.noreply.github.com>
2023-10-09 17:20:02 +0000

    Create alerting.py

---
+++ alerting.py
@@ -0,0 +1,7 @@
+class PacketAlerting:
+    def __init__(self, threshold=100):
+        self.threshold = threshold
+
+    def check_alert(self, packet):
+        # Implement your alerting logic here
+        pass
